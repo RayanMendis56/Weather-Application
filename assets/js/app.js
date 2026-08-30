442c2a312a537a4e5e4b04a7a5176785dcb5a8f1
RayanMendis56 <rayanmendisa13@gmail.com>
2026-08-31 01:07:40 +0530

    add gitignore for config and system files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-const apiKey="4d8d2998b77a6d5b678491a34bf3ade5";
+
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox=document.querySelector(".search input");
